431af1624b187b00e56c4042f50889fb13fb7056
mdb1022 <mdb1022@wildcats.unh.edu>
2017-04-14 15:02:20 -0400

    Change login.php to PDO

--- htdocs/login.php
+++ htdocs/login.php
@@ -21,25 +21,34 @@
     exit;       
   }
 
-	
-	$db = new mysqli( 'localhost', 'MyShows_mdb1022', 'MyShows_mdb1022', 'MyShows_mdb1022');
+  try {
+    $db = new PDO('mysql:host=localhost;dbname=MyShows_mdb1022', 'MyShows_mdb1022', 'MyShows_mdb1022', array (
+      PDO::ATTR_PERSISTENT => true
+    ));
+  }
+  catch(PDOException $e )
+  {
+    header("Location: connectionerror.html");
+    exit;
+  }
 
-	if(mysqli_connect_errno()){
-  	header("Location: connetionerror.html");
-	  exit;
-	}
 
-	$result = $db->query("select * from User
-                         where Username='".$username."'
-                         and Password = sha1('".$password."')");
- 	if (!$result) {
+	$result = $db->prepare("select * from User
+                         where Username=:username
+                         and Password =:password");
+  $result->bindParam(':username', $username );
+  $result->bindParam(':password', sha1($password) );
+  $result->execute();
+  if (!$result)
+  {
    	header("Location: nologin.html");
   	exit;
  	}
-
-	if ($result->num_rows>0) {
-    while($row = $result->fetch_assoc()){
-     $userid = $row["UserID"];
+  if ($result->rowCount() > 0)
+  {
+    foreach( $result->fetchAll(PDO::FETCH_ASSOC) as $row )
+    {
+      $userid = $row["UserID"];
     }
 
     $_SESSION['valid_user'] = $username;
